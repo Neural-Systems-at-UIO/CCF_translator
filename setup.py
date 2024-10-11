47a5bd705e269fd5443e01ee6400f55b2a938682
polarbean <harry.carey95@gmail.com>
2024-10-11 17:48:12 +0200

    update pyppi information

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="CCF_translator",
-    version="0.2",
+    version="0.21",
     packages=find_packages(),
     license="MIT",
     description="a package to translate data between common coordinate templates",
@@ -15,4 +15,14 @@ setup(
     package_data={"CCF_translator": ["metadata/translation_metadata.csv"]},
     include_package_data=True,
     install_requires=["numpy", "nibabel", "scipy", "networkx", "pandas", "requests"],
+    url='https://github.com/Neural-Systems-at-UIO/CCF_translator',
+    author_email='harry.carey@medisin.uio.no',
+    keywords=['neuroinformatics', 'brain', 'atlas', 'brainglobe', 'neuroscience'],
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+],
 )
